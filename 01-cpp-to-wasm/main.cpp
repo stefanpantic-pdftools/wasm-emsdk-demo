d8ae15fb6dc230c0dc3749794290110e4a5b9e51
Stefan Pantic <stefanpantic13@gmail.com>
2023-01-16 08:05:34 +0100

    fix: simplify first example

--- 01-cpp-to-wasm/main.cpp
+++ 01-cpp-to-wasm/main.cpp
@@ -1,17 +1,6 @@
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <numeric>
 
 int main() {
   std::cout << "Hello WASM!" << std::endl;
-
-  std::array<int, 10> numbers;
-  std::iota(numbers.begin(), numbers.end(), 0);
-
-  for (auto num : numbers) {
-    std::cout << num << std::endl;
-  }
-
   return 0;
 }
